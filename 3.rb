87921e9b89679e9d69f4980bafbed6d0a5ae5327
avmxmv <alexandmaximov@gmail.com>
2020-05-21 16:53:30 +0300

    3 commit

--- 3.rb
+++ 3.rb
@@ -1,7 +1,9 @@
 a = gets.chomp.to_i
 b = gets.chomp.to_i
 c = gets.chomp.to_i
-if [a, b, c].max ** 2 == [a, b, c].min ** 2 + (a + b + c - [a, b, c].max - [a, b, c].min) ** 2
+ma = [a, b, c].max
+mi = [a, b, c].min
+if ma**2 == mi**2 + (a + b + c - ma - mi)**2
   puts "Это прямоугольный треугольник"
 end
 if a == b && b == c
--- 4.rb
+++ 4.rb
@@ -1,9 +1,10 @@
-a = gets.chomp.to_i
-b = gets.chomp.to_i
-c = gets.chomp.to_i
+a = gets.chomp.to_f
+b = gets.chomp.to_f
+c = gets.chomp.to_f
 d = b * b - 4 * a * c
+sqrt_from_d = d**0.5
 if d > 0
-  puts (-b + d ** 0.5) / (2 * a), (-b - d ** 0.5) / (2 * a)
+  puts (-b + sqrt_from_d) / (2 * a), (-b - sqrt_from_d) / (2 * a)
 elsif d == 0
   puts -b / (2 * a)
 else

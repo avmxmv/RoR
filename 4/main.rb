f30ee449015d2166b8c78678da716530d9d0b074
avmxmv <alexandmaximov@gmail.com>
2020-06-12 12:52:58 +0300

    Интерфейс

--- 4/main.rb
+++ 4/main.rb
@@ -2,3 +2,10 @@ require_relative 'routes'
 require_relative 'station'
 require_relative 'train'
 require_relative 'modules'
+puts "Добро пожаловать"
+puts "Вы можете:"
+puts "Создать маршрут - Routes.new, и добавлять или удалять в нём станции add_st, del_st."
+puts "Создавать станции - Station.new, искать количесво поездов определённого типа count_trains_by_type, добавлять и удалять станции set_train, unset_train."
+puts "Создавать поезда - Train.new, искать поезда по номеру self.find(number), задавать разные скорости, проверять текущую скороть или останавливать поезд - new_speed(speed), speed_now, stop, а также добавлять и удалять вагоны - add_vagon, del_vagon."
+
+

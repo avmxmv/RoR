f7e34b70571b2d8501af0c6e99a405f0104325c4
avmxmv <alexandmaximov@gmail.com>
2020-06-13 02:46:56 +0300

    Точно добавил

--- 4/main.rb
+++ 4/main.rb
@@ -4,8 +4,50 @@ require_relative 'train'
 require_relative 'modules'
 puts "Добро пожаловать"
 puts "Вы можете:"
-puts "Создать маршрут - Routes.new, и добавлять или удалять в нём станции add_st, del_st."
-puts "Создавать станции - Station.new, искать количесво поездов определённого типа count_trains_by_type, добавлять и удалять станции set_train, unset_train."
-puts "Создавать поезда - Train.new, искать поезда по номеру self.find(number), задавать разные скорости, проверять текущую скороть или останавливать поезд - new_speed(speed), speed_now, stop, а также добавлять и удалять вагоны - add_vagon, del_vagon."
-
-
+puts "1. Создать станцию"
+puts "2. Создать поезд"
+puts "3. Создать маршрут"
+puts "4. Добавить станцию в маршрут"
+puts "5. Удалить станцию из маршрута"
+puts "6. Установить скорость поезду"
+puts "7. Посмотреть текущую скорость поезда"
+puts "8. Остановить поезд"
+for i in range(1..100)
+  puts "Выберите пункт"
+  a = gets.chomp.to_i
+  if a == 1
+    puts "Выберите имя станции"
+    b = gets.chomp.to_str
+    @s = Station.new(b)
+  elsif a == 2
+    puts "Выберите имя поезда"
+    b = gets.chomp.to_str
+    puts "Выберите тип поезда"
+    c = gets.chomp.to_str
+    @t = Train.new(b, c)
+  elsif a == 3
+    puts "Выберите имя начальной станции"
+    b = gets.chomp.to_str
+    puts "Выберите имя конечной станции"
+    c = gets.chomp.to_str
+    @r = Route.new(b, c)
+  elsif a == 4
+    puts "Выберите имя новой станции"
+    c = gets.chomp.to_str
+    @r.add_st(c)
+  elsif a == 5
+    puts "Выберите имя станции для удаления"
+    c = gets.chomp.to_str
+    @r.del_st(c)
+  elsif a == 6
+    puts "Выберите новую скорость поезда"
+    c = gets.chomp.to_i
+    @t.new_speed(c)
+  elsif a == 7
+    @t.speed
+  elsif a == 8
+    @t.stop
+  else
+    puts "Неправильные данные"
+  end
+end

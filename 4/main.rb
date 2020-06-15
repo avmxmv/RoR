8c6c01d134ee261566010a15ade218a76afc1a24
avmxmv <alexandmaximov@gmail.com>
2020-06-15 14:53:03 +0300

    fix 5

--- 4/main.rb
+++ 4/main.rb
@@ -6,76 +6,84 @@ require_relative 'train'
 require_relative 'modules'
 
 class Interface
-  puts 'Добро пожаловать'
-  puts 'Вы можете:'
-  puts '1. Создать станцию'
-  puts '2. Создать поезд'
-  puts '3. Создать маршрут'
-  puts '4. Добавить станцию в маршрут'
-  puts '5. Удалить станцию из маршрута'
-  puts '6. Установить скорость поезду'
-  puts '7. Посмотреть текущую скорость поезда'
-  puts '8. Остановить поезд'
-  trains_arr = []
-  routes_arr = []
-  station_arr = []
-  loop do
-    puts 'Выберите пункт'
-    a = gets.chomp.to_i
-    case a
-    when 1
-      puts 'Выберите имя станции'
-      b = gets.chomp.to_str
-      s = Station.new(b)
-      station_arr << s
-    when 2
-      puts 'Выберите имя поезда'
-      b = gets.chomp.to_str
-      puts 'Выберите тип поезда'
-      c = gets.chomp.to_str
-      t = Train.new(b, c)
-      trains_arr << t
-    when 3
-      puts station_arr
-      puts 'Выберите индекс начальной станции'
-      b = gets.chomp.to_i
-      puts 'Выберите индекс конечной станции'
-      c = gets.chomp.to_i
-      r = Route.new(station_arr[b], station_arr[c])
-      routes_arr << r
-    when 4
-      puts routes_arr
-      puts 'Выберите имя новой станции'
-      c = gets.chomp.to_str
-      puts 'Выберите индекс маршрута'
-      b = gets.chomp.to_i
-      routes_arr[b].add_st(c)
-    when 5
-      puts station_arr
-      puts 'Выберите индекс станции'
-      b = gets.chomp.to_i
-      puts 'Выберите имя станции для удаления'
-      c = gets.chomp.to_str
-      station_arr[b].del_st(c)
-    when 6
-      puts trains_arr
-      puts 'Выберите индекс поезда'
-      b = gets.chomp.to_i
-      puts 'Выберите новую скорость поезда'
-      c = gets.chomp.to_i
-      trains_arr[b].new_speed(c)
-    when 7
-      puts trains_arr
-      puts 'Выберите индекс поезда'
-      b = gets.chomp.to_i
-      trains_arr[b].speed
-    when 8
-      puts trains_arr
-      puts 'Выберите индекс поезда'
-      b = gets.chomp.to_i
-      trains_arr[b].stop
-    else
-      puts 'Неправильные данные'
+  def initialize
+    # код который выполняется при создании объекта
+    @trains = []
+    @routes = []
+    @stations = []
+  end
+
+  def run # главный запуск
+    loop do
+      help # вызов информации выбора каждый раз
+      choice  = gets.chomp.to_i
+      case choice
+      when 1
+        puts 'Введите имя станции'
+        name = gets.chomp.to_str
+        @station << Station.new(name)
+      when 2
+        puts 'Введите имя поезда'
+        name = gets.chomp.to_str
+        puts 'Выберите тип поезда'
+        type = gets.chomp.to_str
+        trains_arr << Train.new(name, type)
+      when 3
+        puts station_arr
+        puts 'Введите индекс начальной станции'
+        index1 = gets.chomp.to_i
+        puts 'Введите индекс конечной станции'
+        index2 = gets.chomp.to_i
+        routes_arr << Route.new(station_arr[index1], station_arr[index2])
+      when 4
+        puts routes_arr
+        puts 'Введите имя новой станции'
+        name = gets.chomp.to_str
+        puts 'Введите индекс маршрута'
+        index = gets.chomp.to_i
+        routes_arr[index].add_st(name)
+      when 5
+        puts station_arr
+        puts 'Введите индекс станции'
+        index = gets.chomp.to_i
+        puts 'Введите имя станции для удаления'
+        name = gets.chomp.to_str
+        station_arr[index].del_st(name)
+      when 6
+        puts trains_arr
+        puts 'Введите индекс поезда'
+        index = gets.chomp.to_i
+        puts 'Введите новую скорость поезда'
+        speed = gets.chomp.to_i
+        trains_arr[index].new_speed(speed)
+      when 7
+        puts trains_arr
+        puts 'Введите индекс поезда'
+        index = gets.chomp.to_i
+        trains_arr[index].speed
+      when 8
+        puts trains_arr
+        puts 'Введите индекс поезда'
+        index = gets.chomp.to_i
+        trains_arr[index].stop
+      else
+        puts 'Неправильные данные'
+      end
     end
   end
+
+  def help # информация
+    puts 'Добро пожаловать'
+    puts 'Вы можете:'
+    puts '1. Создать станцию'
+    puts '2. Создать поезд'
+    puts '3. Создать маршрут'
+    puts '4. Добавить станцию в маршрут'
+    puts '5. Удалить станцию из маршрута'
+    puts '6. Установить скорость поезду'
+    puts '7. Посмотреть текущую скорость поезда'
+    puts '8. Остановить поезд'
+  end
 end
+
+Interface.new.run

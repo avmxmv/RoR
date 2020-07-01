7a1d68ee4918cb7236ad48b55117fce4b9e9ece3
avmxmv <alexandmaximov@gmail.com>
2020-07-01 14:20:29 +0300

    fix 16

--- 4/acсessor.rb
+++ 4/acсessor.rb
@@ -8,4 +8,10 @@ module Acсessors
       define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
     end
   end
+
+  def strong_attr_accessor(name)
+    var_name = "@#{name}".to_sym
+    define_method(name) { instance_variable_get(var_name) }
+    define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
+  end
 end

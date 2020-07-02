088b729bc82ef9f42db08fa77a2bdb33d957e57b
avmxmv <alexandmaximov@gmail.com>
2020-07-02 16:45:35 +0300

    fix 17

--- 4/validation.rb
+++ 4/validation.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 module Validation
-  def validate!
-    raise 'Неверный формат номера поезда' if @number !~ /^[a-zA-Z0-9]{3}-?[a-zA-Z0-9]{2}$/
-  end
-
-  def valid?
-    validate!
-    true
-  rescue StandardError
-    puts 'Попробуйте ещё раз'
-    false
-  end
+  validate :name, :presence
+  validate :name, :format, /^[a-zA-Z0-9]{3}-?[a-zA-Z0-9]{2}$/
+  validate :name, :type, String
 end
